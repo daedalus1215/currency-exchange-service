50867ab33fb316d2b12335f3ffdce69b717c5f0e
larry.adams <ladams1776@yahoo.com>
2019-09-13 05:58:33 -0400

    Fix the sql data script and updated the Entity

--- src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,7 +1,7 @@
 package com.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
@@ -9,7 +9,9 @@ import java.math.BigDecimal;
 public class ExchangeValue {
     @Id
     private Long id;
+    @Column(name="currency_from")
     private String from;
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
