1eb9a59ea54bc32d620cf81e49e2a19321df8863
larry.adams <ladams1776@yahoo.com>
2019-09-13 05:42:41 -0400

    have config display port and set port to 8000

--- src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,6 @@
 package com.microservices.currencyexchangeservice;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    private Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        final ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
     }
 }
