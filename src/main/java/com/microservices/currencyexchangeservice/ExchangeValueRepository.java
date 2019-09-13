a4fc785e988e81287333c93ceeba4fdac31cdfbf
larry.adams <ladams1776@yahoo.com>
2019-09-13 06:03:46 -0400

    Add new find method on Repo Interface

--- src/main/java/com/microservices/currencyexchangeservice/ExchangeValueRepository.java
+++ src/main/java/com/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -3,4 +3,5 @@ package com.microservices.currencyexchangeservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+    ExchangeValue findByFromAndTo(String from, String to);
 }
